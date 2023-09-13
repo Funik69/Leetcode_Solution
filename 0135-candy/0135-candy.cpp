9e1d9562eeef65d402c04b87d47539f03785070a
Raghav Upadhyay <90557042+Funik69@users.noreply.github.com>
2023-09-13 10:45:17 +0530

    Time: 17 ms (62.3%), Space: 18.2 MB (24.67%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int candy(vector<int>& rate) {
+        int sum=0;
+        int n=rate.size();
+        vector<int> pre(n,1),suf(n,1);
+        for(int i=1;i<n;i++){
+            if(rate[i]>rate[i-1]) pre[i]=pre[i-1]+1;
+        }
+        for(int i=n-2;i>=0;i--){
+            if(rate[i]>rate[i+1]) suf[i]=suf[i+1]+1;
+        }
+        for(int i=0;i<n;i++){
+            sum+=max(pre[i],suf[i]);
+        }
+        return sum;
+    }
+};
