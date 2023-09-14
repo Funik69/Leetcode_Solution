57c544787a3714bde940f3e4a5d9afa17ae3b609
Raghav Upadhyay <90557042+Funik69@users.noreply.github.com>
2023-09-14 23:03:54 +0530

    Time: 8 ms (50.58%), Space: 6.4 MB (40.66%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        string str="";
+        while(num>=1000){
+            str+="M";
+            num-=1000;
+        }
+        if(num>=900){
+            str+="CM";
+            num-=900;
+        }
+        while(num>=500){
+            str+="D";
+            num-=500;
+        }
+        if(num>=400){
+            str+="CD";
+            num-=400;
+        }
+        while(num>=100){
+            str+="C";
+            num-=100;
+        }
+        if(num>=90){
+            str+="XC";
+            num-=90;
+        }
+        while(num>=50){
+            str+="L";
+            num-=50;
+        }
+        if(num>=40){
+            str+="XL";
+            num-=40;
+        }
+        while(num>=10){
+            str+="X";
+            num-=10;
+        }
+        if(num>=9){
+            str+="IX";
+            num-=9;
+        }
+        while(num>=5){
+            str+="V";
+            num-=5;
+        }
+        if(num>=4){
+            str+="IV";
+            num-=4;
+        }
+        while(num>=1){
+            str+="I";
+            num--;
+        }
+        return str;
+    }
+};
