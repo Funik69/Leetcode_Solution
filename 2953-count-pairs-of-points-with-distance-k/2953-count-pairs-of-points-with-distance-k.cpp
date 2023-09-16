908d27e36b138a7ba640e2734c76f5a73d1ff8b3
Raghav Upadhyay <90557042+Funik69@users.noreply.github.com>
2023-09-16 22:38:16 +0530

    Time: 2670 ms (50%), Space: 390.3 MB (80%) - LeetHub

--- 2953-count-pairs-of-points-with-distance-k/2953-count-pairs-of-points-with-distance-k.cpp
+++ 2953-count-pairs-of-points-with-distance-k/2953-count-pairs-of-points-with-distance-k.cpp
@@ -4,7 +4,10 @@ public:
         map<pair<int,int>,int> mp;
         int cnt=0;
         for(auto i:c){
+            //if , c[i][0]^x=val                      
+            //then c[i][1]^y=k-val    
             for(int j=0;j<=k;j++){
+                //a^b=c then a^c=b
                 int x=j^i[0];
                 int y=(k-j)^i[1];
                 cnt+=mp[{x,y}];
