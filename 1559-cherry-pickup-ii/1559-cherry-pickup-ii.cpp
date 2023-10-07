5dbbe230aca5e864671f97465fb3d604744b592f
Raghav Upadhyay <90557042+Funik69@users.noreply.github.com>
2023-10-07 12:52:10 +0530

    Time: 123 ms (29.91%), Space: 17.5 MB (13.97%) - LeetHub

--- 1559-cherry-pickup-ii/1559-cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/1559-cherry-pickup-ii.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
 int soll(int i,int j1,int j2,int m,int n,vector<vector<int>> & grid,vector<vector<vector<int>>> & dp){
+    //out of bound condition
     if(j1<0 or j1>=n or j2<0 or j2>=n) return INT_MIN;
+    //last row base case
     if(i==m-1){
         if(j1==j2) return grid[i][j1];
         else return grid[i][j1]+grid[i][j2];
     }
+    //if already found sol
     if(dp[i][j1][j2]!=-1) return dp[i][j1][j2];
     int mx=INT_MIN;
+    //9 possible combination
     for(int dj1=-1;dj1<=1;dj1++){
         for(int dj2=-1;dj2<=1;dj2++){
             int val = 0 ;
+            //both reach to same cell
             if(j1==j2) val = grid[i][j1];
             else val = grid[i][j1]+grid[i][j2];
             val+=soll(i+1,j1+dj1,j2+dj2,m,n,grid,dp);
